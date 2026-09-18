0ca13c9b013eba933bf3a19fc2cd2e98fc1f4dba
Sakshi Bansal <sakshibansal827@gmail.com>
2026-09-19 00:13:36 +0530

    feat: initialize client and server structure with Firebase authentication
    - Added client package.json with dependencies for React, TailwindCSS, and Vite.
    - Created App component for Google login using Firebase authentication.
    - Set up main entry point for React application.
    - Configured Axios for API requests with base URL from environment variables.
    - Initialized Firebase in the server with service account credentials.
    - Implemented user model and authentication controller for handling login requests.
    - Established routes for authentication and integrated with Express.
    - Added .gitignore files for client and server to exclude node_modules and environment files.

--- server/services/auth/index.js
+++ server/services/auth/index.js
@@ -1,17 +1,19 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectdb from "./config/db.js";
+import router from "./routes/auth.route.js"
 dotenv.config();
 
-const port = process.env.PORT;
+const port = process.env.PORT
 
 const app = express();
 
-
+app.use(express.json());
+app.use("/",router)
 app.get("/",(req,res)=>{
     res.json({message:"hello from server"});
 })
 app.listen(port,()=>{
-    console.log(`auth server started at ${port}`);
+    console.log(`auth server started at ${port}`)
     connectdb();
 })
---
+++ server/services/auth/models/user.model.js
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose';
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    avatar: {
+        type: String,
+    },
+    firebaseUid: {
+        type: String,
+        required: true,
+    }
+}, { timestamps: true });
+export const User = mongoose.model('User', userSchema);
